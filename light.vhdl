66f1a792d6bc351a78a4a5d0d9765f3bbaeee448
Isac E Perez <89553126+Isac-E-Perez@users.noreply.github.com>
2021-10-13 19:58:17 -0500

    Create light.vhdl

---
+++ light.vhdl
@@ -0,0 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity light is 
+  port(
+    SW2, SW1, SW0 : in std_logic;
+    LED1, LED0 : out std_logic
+    );
+end light
+  
+architecture behavior of light is 
+  
+  signal A, B, C : std_logic;
+  signal x, y, f : std_logic;
+
+begin  
+  
+  A <= SW2;
+  B <= SW1;
+  C <= SW0;
+
+  x <= not(A) and (B xor C);
+  y <= A and (not(B) and not(C));
+  LED1 <= x or y;
+  LED0 <= not(A) or not(B) or not(C);
+
+end behavior; 
